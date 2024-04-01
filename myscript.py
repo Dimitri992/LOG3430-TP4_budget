e22638911663f54ce29880a8caf3258c16f1911d
ismailes <ismail.essid09@gmail.com>
2024-03-31 22:59:25 -0400

    partie C change to main.yaml 2

--- myscript.py
+++ myscript.py
@@ -1,5 +1,4 @@
 import os
-import sys
 goodHash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
 badHash = "c1a4be04b972b6c17db242fc37752ad517c29402"
 os.system(f'git bisect start {badHash} {goodHash}')
