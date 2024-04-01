8540572a1cd7e776d1257555f05fe3a5971cccb9
ismailes <ismail.essid09@gmail.com>
2024-03-31 22:39:31 -0400

    partie C

---
+++ myscript.py
@@ -0,0 +1,7 @@
+import os
+import sys
+goodHash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
+badHash = "c1a4be04b972b6c17db242fc37752ad517c29402"
+os.system(f'git bisect start {badHash} {goodHash}')
+os.system('git bisect run python manage.py test')
+os.system('git bisect reset')
